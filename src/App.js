85351a500014673dd4003cb0b836901609bbaf2d
msaif2729 <saiiifansari@gmail.com>
2024-08-09 21:54:45 +0530

    Frontend Setup Complented

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="">
-    Hello world!
-   </h1>
+    <>
+      <h1 className=' text-3xl text-red-500 font-sans font-semibold'>
+        Mohammed Saif
+      </h1>
+    </>
   );
 }
 
